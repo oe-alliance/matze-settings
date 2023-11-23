aca4b2bb1fc3407c0775253153c316eaf879b611
jbleyel <jbleyel@gmx.net>
2023-11-23 17:08:13 +0100

    fix branch

--- CI/build.sh
+++ CI/build.sh
@@ -36,7 +36,7 @@ setup_git() {
 }
 
 commit_files() {
-  git checkout master
+  git checkout main
   git add -u
   git add *
   git commit --message "Fetch latest Matze settings."
@@ -46,7 +46,7 @@ commit_files() {
 
 upload_files() {
   git remote add upstream https://${GITHUB_TOKEN}@github.com/oe-alliance/matze-settings.git > /dev/null 2>&1
-  git push --quiet upstream master || echo "failed to push with error $?"
+  git push --quiet upstream main || echo "failed to push with error $?"
 }
 
 setup_git
